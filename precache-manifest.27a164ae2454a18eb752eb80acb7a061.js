c37fa88449e4ddd31d97cda48ce12e9e9163c05b
Saket Verma <saketverma@plexusmd.com>
2020-08-03 00:24:56 +0530

    Updates

---
+++ precache-manifest.27a164ae2454a18eb752eb80acb7a061.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4b6311f904b1ba48788f989b0d2f57",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e85793fa36ace0b09904",
+    "url": "/Portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a075e65efea9ec31c44a",
+    "url": "/Portfolio/static/css/main.fdc0bb66.chunk.css"
+  },
+  {
+    "revision": "e85793fa36ace0b09904",
+    "url": "/Portfolio/static/js/2.1495b8f1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio/static/js/2.1495b8f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a075e65efea9ec31c44a",
+    "url": "/Portfolio/static/js/main.12905e29.chunk.js"
+  },
+  {
+    "revision": "93a3f239049b9da02d65",
+    "url": "/Portfolio/static/js/runtime-main.8aba824b.js"
+  },
+  {
+    "revision": "ed69a0fffa457de42e0e74ba963545a9",
+    "url": "/Portfolio/static/media/myAvatar.ed69a0ff.svg"
+  },
+  {
+    "revision": "8e3e472fcd2f01ba9e364f8a798cad42",
+    "url": "/Portfolio/static/media/sr_mukul.8e3e472f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.89d4a5d2ed8ba606695c7e1f552404bf.js"
+  "/Portfolio/precache-manifest.27a164ae2454a18eb752eb80acb7a061.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
